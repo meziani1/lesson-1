813a1090421986fb29f4cf3f3f4df2f7b1427e6b
Youssef MEZIANI <youssef12era@gmail.com>
2024-06-10 18:49:52 +0100

    lesson2

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // Import the Express module
 const express = require('express');
+const mongoose = require('mongoose');
 
 // Create an instance of an Express application
 const app = express();
@@ -12,7 +13,13 @@ app.get('/',(req,res)=>{
   res.sendFile("./views/style/style.html", { root: __dirname })
 })
 
-// Start the server and listen on the defined port
-app.listen(PORT, () => {
-  console.log(`http://localhost:${PORT}`);
-});
+
+
+mongoose.connect("mongodb+srv://youssefDEV:cljEcDR6tpcMDSoM@cluster0.ccpqlf1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+                        .then(() => { // Start the server and listen on the defined port
+                             app.listen(PORT, () => {
+                            console.log(`http://localhost:${PORT}`);
+                          }); })
+                        .catch((err) => {console.log(err)})
+  
+
